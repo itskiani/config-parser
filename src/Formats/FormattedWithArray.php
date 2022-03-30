f973621598095aeb91c67b90e3abffcd58b04b85
ali <codewithkiani@gmail.com>
2022-03-30 18:30:20 +0430

    add all classes for different formats

---
+++ src/Formats/FormattedWithArray.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace ItsKiani\ConfigParser\Formats;
+
+use ItsKiani\ConfigParser\Contracts\Parser;
+
+class FormattedWithArray implements Parser
+{
+
+    public function parse()
+    {
+        // TODO: Implement parse() method.
+    }
+}
---
+++ src/Formats/FormattedWithJSON.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace ItsKiani\ConfigParser\Formats;
+
+use ItsKiani\ConfigParser\Contracts\Parser;
+
+class FormattedWithJSON implements Parser
+{
+
+    public function parse()
+    {
+        // TODO: Implement parse() method.
+    }
+}
