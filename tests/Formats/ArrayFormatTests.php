a9ab72aca5f54ac5bf93ac93c0e12e67e6d78842
ali <codewithkiani@gmail.com>
2022-04-19 14:49:28 +0430

    update ArrayFormatTests

--- tests/Formats/ArrayFormatTests.php
+++ tests/Formats/ArrayFormatTests.php
@@ -7,13 +7,19 @@ use ItsKiani\ConfigParser\Tests\TestCase;
 
 class ArrayFormatTests extends TestCase
 {
+    protected string $baseUrl = 'http://localhost:8800';
     protected object $arrayFormat;
 
-    protected function set_up()
+    protected function setUp(): void
     {
         $this->arrayFormat = new FormattedWithArray([
             'app' => __DIR__.'/../Mocks/Config/app.php',
         ]);
+    }
+
+    public function tearDown(): void
+    {
+        unset($this->arrayFormat);
     }
 
     /**
